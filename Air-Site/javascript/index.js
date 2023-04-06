9f534507376c3beda936dda1bfea2d872973a472
Randeep Singh <rs513535@gmail.com>
2023-04-06 20:06:32 +0530

    some changes

--- Air-Site/javascript/index.js
+++ Air-Site/javascript/index.js
@@ -12,12 +12,7 @@ window.addEventListener('scroll', ()=>{
     if (currentScroll <= 0) {
         body.classList.remove('scroll-up');
     }
-    if(currentScroll > lastScroll && !body.classList.contains('scroll-down')){
-       body.classList.remove('scroll-up');
-        body.classList.add('scroll-down')
-    }
-    if(currentScroll < lastScroll && body.classList.contains('scroll-down')){
-        body.classList.remove('scroll-down');
+    if (currentScroll >= lastScroll) {
         body.classList.add('scroll-up')
     }
     lastScroll=currentScroll
